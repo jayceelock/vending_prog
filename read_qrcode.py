4563f0a70019d80fa9997056ac03edf0e437c944
jayceelock <jaycee.lock@gmail.com>
2013-09-04 16:39:41 +0200

    Fixed ZBar_cam bombing out after one transaction

--- read_qrcode.py
+++ read_qrcode.py
@@ -7,60 +7,93 @@ import zbar
 
 #while 1:
 def read(challenge_code):
-    # create a Processor
-    proc = zbar.Processor()
+#     # create a Processor
+#     proc = zbar.Processor()
+# 
+#     # configure the Processor
+#     proc.parse_config('enable')
+# 
+#     # initialize the Processor
+#     device = '/dev/video0'
+#     if len(argv) > 1:
+#         device = argv[1]
+#     proc.init(device)
+#     
+#     # enable the preview window
+#     proc.visible = True
+# 
+#     # read at least one barcode (or until window closed)
+#     proc.process_one()
+#     
+#     # hide the preview window
+#     proc.visible = False
+#     
+#     # extract results
+#     for symbol in proc.results:
+#         # do something useful with results
+#         verify_code = symbol.data
 
+        # create a Processor
+    proc = zbar.Processor()
+    
     # configure the Processor
     proc.parse_config('enable')
-
+    
     # initialize the Processor
     device = '/dev/video0'
     if len(argv) > 1:
         device = argv[1]
     proc.init(device)
     
+    # setup a callback
+    def my_handler(proc, image, closure):
+        # extract results
+        for symbol in image:
+            if not symbol.count:
+                # do something useful with results
+                process_code(symbol.data, challenge_code)
+                #print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+    
+    proc.set_data_handler(my_handler)
+    
     # enable the preview window
     proc.visible = True
-
-    # read at least one barcode (or until window closed)
-    proc.process_one()
     
-    # hide the preview window
-    proc.visible = False
-    
-    # extract results
-    for symbol in proc.results:
-        # do something useful with results
-        verify_code = symbol.data
+    # initiate scanning
+    proc.active = True
+    try:
+        proc.process_one()
+        #proc.user_wait()
+    except zbar.WindowClosed:
+        pass
      
+     
+def process_code(verify_code, challenge_code):     
+    
     server_pub_key = Pickle.load(open("server_key.pub", 'r'))
-     
+
     vending_priv_key = Pickle.load(open("vending_key.priv", 'r'))
-     
+
     verify_code = verify_code.split('[]', 2)
     encoded_string = verify_code[0].split('**', 2)
     encoded_signature = verify_code[1].split('**', 2)
-     
+
     encrypted_string = (base64.b64decode(encoded_string[0]), base64.b64decode(encoded_string[1]))
     signature = (long(base64.b64decode(encoded_signature[0])), long(base64.b64decode(encoded_signature[1])))
-    import pdb; pdb.set_trace();    
-     
     
-    #decoded_message = base64.b64decode(data_block_1)
-    #decoded_signature = base64.b64decode(data_block_2)
-    
+    #import pdb; pdb.set_trace();
     decrypted_message = vending_priv_key.decrypt(encrypted_string)
-    #encrypted_string = encrypted_string.strip('QR-Code:')
+    #encrypted_string = encrypted_string.strip('decoded QRCODE symbol')
     hash_code = MD5.new(decrypted_message[4:8]).digest()
     
     verify_test = server_pub_key.verify(hash_code, signature)
-    
+
     if decrypted_message[4:8] != challenge_code:
         print 'Invalid QR-Code. Please try again.'
         if verify_test != True:
             print 'The source of this QR-code could not be verified. Please try again.'
         #return None
-    
+
     else:
         print "Thank you for your purchase. Enjoy your day :)"
     #break
--- read_qrcode_2.py
+++ read_qrcode_2.py
@@ -1,53 +1,59 @@
-import subprocess
-import time
-import os
-import base64
-
-from Crypto.PublicKey import RSA 
+#!/usr/bin/python
 from Crypto.Hash import MD5
+from sys import argv
+import base64
+import cPickle as Pickle
+import zbar
 
-from ezPyCrypto import key
-
+#while 1:
 def read(challenge_code):
-    while True:
-        os.system("cd /media/J-Drive/skripsie/vending_prog/barcodes/")
-        os.system("fswebcam /media/J-Drive/skripsie/vending_prog/barcodes/test.jpg -q -r 320x240")
-        time.sleep(3)
-        try:
-            verify_code = subprocess.check_output(["zbarimg", "/media/J-Drive/skripsie/vending_prog/barcodes/test.jpg", "-q"])
-            print verify_code
-            break
-        except:
-            print 'No code found'
-        
-    server_pub_key = key()
-    vending_priv_key = key()
+    # create a Processor
+    proc = zbar.Processor()
+
+    # configure the Processor
+    proc.parse_config('enable')
+
+    # initialize the Processor
+    device = '/dev/video0'
+    if len(argv) > 1:
+        device = argv[1]
+    proc.init(device)
+    
+    # enable the preview window
+    proc.visible = True
+
+    # read at least one barcode (or until window closed)
+    proc.process_one()
+    
+    # hide the preview window
+    proc.visible = False
+    
+    # extract results
+    for symbol in proc.results:
+        # do something useful with results
+        verify_code = symbol.data
      
-    key_file = open('server_key_ez.pub')
-    server_pub_key_f = key_file.read()
-    key_file.close()
+    server_pub_key = Pickle.load(open("server_key.pub", 'r'))
      
-    key_file = open('vending_key_ez.priv')
-    vending_priv_key_f = key_file.read()
-    key_file.close()
-     
-    server_pub_key.importKey(server_pub_key_f)
-    vending_priv_key.importKey(vending_priv_key_f)
+    vending_priv_key = Pickle.load(open("vending_key.priv", 'r'))
      
     verify_code = verify_code.split('[]', 2)
+    encoded_string = verify_code[0].split('**', 2)
+    encoded_signature = verify_code[1].split('**', 2)
      
-    data_block_1 = verify_code[0]
-    data_block_2 = verify_code[1]
+    encrypted_string = (base64.b64decode(encoded_string[0]), base64.b64decode(encoded_string[1]))
+    signature = (long(base64.b64decode(encoded_signature[0])), long(base64.b64decode(encoded_signature[1])))
+    import pdb; pdb.set_trace();    
+     
     
-    data_block_1 = data_block_1.strip('QR-Code:') 
-    #data_block_1 = '<StartPyCryptoMessage>\n' + data_block_1 + '\n<EndPryCryptoMessage>'
-    data_block_2 = '<StartPycryptoSignature>\n' + data_block_2 + '<EndPycryptoSignature>'
+    #decoded_message = base64.b64decode(data_block_1)
+    #decoded_signature = base64.b64decode(data_block_2)
     
+    decrypted_message = vending_priv_key.decrypt(encrypted_string)
+    #encrypted_string = encrypted_string.strip('QR-Code:')
+    hash_code = MD5.new(decrypted_message[4:8]).digest()
     
-    decoded_message = base64.b64decode(data_block_1)
-    decrypted_message = vending_priv_key.decString(decoded_message)
-    import pdb;pdb.set_trace()
-    verify_test = server_pub_key.verifyString(decrypted_message, data_block_2)
+    verify_test = server_pub_key.verify(hash_code, signature)
     
     if decrypted_message[4:8] != challenge_code:
         print 'Invalid QR-Code. Please try again.'
@@ -57,30 +63,4 @@ def read(challenge_code):
     
     else:
         print "Thank you for your purchase. Enjoy your day :)"
-    
-
-    
-#===============================================================================
-# server_pub_key_file = open('server_key.pub')
-# server_pub_key = RSA.importKey(server_pub_key_file.read())
-# server_pub_key_file.close()
-#     
-# vending_priv_key_file = open("vending_key.priv")
-# vending_priv_key = RSA.importKey(vending_priv_key_file.read())
-# vending_priv_key_file.close()
-#     
-# encoded_data = verify_code.split("[]", 2)
-# import pdb;pdb.set_trace()    
-# encoded_data_block_1 = encoded_data[0].strip("QR-Code:")
-# encoded_data_block_2 = encoded_data[1]
-#     
-# decoded_data_block_1 = base64.b64decode(encoded_data_block_1)
-# decoded_data_block_2 = base64.b64decode(encoded_data_block_2)
-#     
-# decrypted_product_code = vending_priv_key.decrypt(decoded_data_block_1)
-# decrypted_verify_code = decrypted_product_code.split('&')[1]
-#  
-# hash_code = MD5.new(decrypted_verify_code).digest()
-# verify_test = server_pub_key.verify(hash_code, [long(decoded_data_block_2), ])
-#===============================================================================
-
+    #break
