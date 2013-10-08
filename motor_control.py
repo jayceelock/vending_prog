d0ec99b9200cb6f1ea2602b1696f7ec3de5011ac
jayceelock <jaycee.lock@gmail.com>
2013-10-08 13:23:46 +0200

    awe

--- motor_control.py
+++ motor_control.py
@@ -3,19 +3,21 @@ import time
 
 def motor_switch(motor):
     print('Activating motor' + str(motor))
+    
     GPIO.setmode(GPIO.BOARD)
-     
+  
     GPIO.setup(12, GPIO.OUT)
     GPIO.setup(16, GPIO.OUT)
-     
+      
     if motor == 1:
         GPIO.output(12, GPIO.HIGH)
         time.sleep(5)
         GPIO.output(12, GPIO.LOW)
-         
+          
     if motor == 2:
         GPIO.output(16, GPIO.HIGH)
         time.sleep(5)
         GPIO.output(16, GPIO.LOW)
+
     
     
--- vending_gui.py
+++ vending_gui.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-
+"""
+This is the GUI script for the vending machine. It uses WX Python to
+create a GUI that provides the customer with payment and product options. 
+"""
 import wx
+
 
 from generate_qrcode import make_qrcode
 from encrypt_elgamal import encrypt_code
@@ -11,21 +15,25 @@ from pay_stud import process_card
  
 ########################################################################
 class MainPanel(wx.Panel):
-    """"""
- 
-    #----------------------------------------------------------------------
+    """
+    The main class of the GUI. Forms the main frame in which all the other elements are placed
+    """
+
     def __init__(self, parent):
-        """Constructor"""
+        """Frame constructor"""
         wx.Panel.__init__(self, parent=parent)
         self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
         self.frame = parent
- 
+        
+        """Add sizer to frame"""
         sizer = wx.BoxSizer(wx.VERTICAL)
         hSizer = wx.BoxSizer(wx.HORIZONTAL)
         
+        """Add black picture as default"""
         bmp = wx.Image("black.jpg", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
         wx.StaticBitmap(self, -1, bmp, pos=(10, 10), size=(413, 400))
         
+        """Create read-only text boxes to communicate with user"""
         self.dialog = wx.TextCtrl(self, style = wx.TE_READONLY|wx.TE_MULTILINE, size = (200, 90))
         self.dialog.SetValue("")
         self.info1 = wx.TextCtrl(self, style = wx.TE_READONLY|wx.TE_MULTILINE, size = (200, 55))
@@ -33,6 +41,7 @@ class MainPanel(wx.Panel):
         self.info2 = wx.TextCtrl(self, style = wx.TE_READONLY|wx.TE_MULTILINE, size = (200, 50))
         self.info2.SetValue("Please select a payment option from the list below")
         
+        """Create buttons"""
         coke_button = wx.Button(self, label = "Coca Cola (340ml)")
         lays_button = wx.Button(self, label = "Lay's Lightly Salted (35g)")
         qr_button = wx.Button(self, label = "Pay using QR Codes")
@@ -40,6 +49,7 @@ class MainPanel(wx.Panel):
         nfc_button = wx.Button(self, label = "Pay using NFC")
         stud_button = wx.Button(self, label = "Pay using SU Card")
         
+        """Add event listeners to buttons"""
         coke_button.Bind(wx.EVT_BUTTON, self.OnCoke)
         lays_button.Bind(wx.EVT_BUTTON, self.OnLays)
         continue_button.Bind(wx.EVT_BUTTON, self.OnContinue)
@@ -47,6 +57,7 @@ class MainPanel(wx.Panel):
         stud_button.Bind(wx.EVT_BUTTON, self.OnStud)
         qr_button.Bind(wx.EVT_BUTTON, self.OnQrCode)
         
+        """Add all elements to sizer"""
         sizer.Add(self.info1, 0, wx.ALL, 5)
         sizer.Add(coke_button, 0, wx.ALL, 5)
         sizer.Add(lays_button, 0, wx.ALL, 5)
@@ -57,56 +68,42 @@ class MainPanel(wx.Panel):
         sizer.Add(stud_button, 0, wx.ALL, 5)
         sizer.Add(self.dialog, 0, wx.ALL, 5)
         
+        """Add sizers to frame"""
         hSizer.Add((1,1), 1, wx.EXPAND)
         hSizer.Add(sizer, 0, wx.TOP, 10)
         hSizer.Add((1,1), 0, wx.ALL, 10)
         self.SetSizer(hSizer)
-        #self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
- 
-    #----------------------------------------------------------------------
-    def OnEraseBackground(self, evt):
-        """
-        Add a picture to the background
-        """
-        # yanked from ColourDB.py
-        dc = evt.GetDC()
- 
-        if not dc:
-            dc = wx.ClientDC(self)
-            rect = self.GetUpdateRegion().GetBox()
-            dc.SetClippingRect(rect)
-        dc.Clear()
-        bmp = wx.Bitmap("qrcode.png")
-        dc.DrawBitmap(bmp, 0, 0)
- 
-    def OnCoke(self, e):
-        global challenge, motor, qr_prod_code
+    
+    """
+    Specify what happens when user clicks specific button
+    """
+    
+    def OnCoke(self, e):            #Specify correct motor and product code 
+        global qr_prod_code, motor
         
         qr_prod_code = 'A061'
                 
         motor = 1
         
-    def OnLays(self, e):
-        global challenge, motor, qr_prod_code
+    def OnLays(self, e):            #Specify correct motor and product code
+        global qr_prod_code, motor
         
         qr_prod_code = '233C'
         
         motor = 2
 
-    def OnQrCode(self, e):
+    def OnQrCode(self, e):          #Create QR Code and display it
         global qr_prod_code, challenge
         
-        url, challenge = encrypt_code('233C')
+        url, challenge = encrypt_code(qr_prod_code)
         make_qrcode(url)
         
         bmp = wx.Image("qrcode.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-        wx.StaticBitmap(self, -1, bmp, pos=(10, 10), size=(420, 500))
-        
-        #print 'awe'    
+        wx.StaticBitmap(self, -1, bmp, pos=(10, 10), size=(420, 500))   
         
     def OnContinue(self, e):
         
-        global motor
+        global motor, challenge
         read(challenge, motor)
         
         self.dialog.SetValue("")
@@ -134,29 +131,26 @@ class MainPanel(wx.Panel):
             motor_switch(motor)
             print uid
             
-########################################################################
 class MainFrame(wx.Frame):
     """"""
  
-    #----------------------------------------------------------------------
     def __init__(self):
         """Constructor"""
         wx.Frame.__init__(self, None, size=(700,550))
         panel = MainPanel(self)        
         self.Center()
  
-########################################################################
 class Main(wx.App):
-    """"""
+    """
+    Make the GUI callable from a terminal command line
+    """
  
-    #----------------------------------------------------------------------
     def __init__(self, redirect=False, filename=None):
         """Constructor"""
         wx.App.__init__(self, redirect, filename)
         dlg = MainFrame()
         dlg.Show()
  
-#----------------------------------------------------------------------
 if __name__ == "__main__":
     app = Main()
     app.MainLoop()
