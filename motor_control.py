eb6f72ed6334aec07834ccbb68360515ef50b99b
jayceelock <jaycee.lock@gmail.com>
2013-10-22 11:01:36 +0200

    Adding comments

--- motor_control.py
+++ motor_control.py
@@ -1,9 +1,15 @@
+"""
+This is the motor_control module of the vending machine. It controls the motors 
+using the GPIO from the Raspberry Pi.
+"""
+
 import RPi.GPIO as GPIO
 import time
 
 def motor_switch(motor):
     print('Activating motor' + str(motor))
     
+    #Set the GPIO's to output mode
     GPIO.setmode(GPIO.BOARD)
   
     GPIO.setup(12, GPIO.OUT)
@@ -11,12 +17,12 @@ def motor_switch(motor):
       
     if motor == 1:
         GPIO.output(12, GPIO.HIGH)
-        time.sleep(5)
+        time.sleep(2)
         GPIO.output(12, GPIO.LOW)
           
     if motor == 2:
         GPIO.output(16, GPIO.HIGH)
-        time.sleep(5)
+        time.sleep(2)
         GPIO.output(16, GPIO.LOW)
 
     
--- pay_stud.py
+++ pay_stud.py
@@ -1,11 +1,17 @@
+"""
+This is the pay_stud module. It allows the vending machine to process payments 
+made via RFID card using the libnfc library.
+"""
+
 import subprocess
 
 def process_card():
-    
+    #Read the RFID card's UID using the nfc-poll module from libfc
     process = subprocess.Popen('nfc-poll', stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
     
     out, err = process.communicate()
     
+    #Process the output and make it pretty
     out = out.split('\n')
     
     uid_line = out[5]
